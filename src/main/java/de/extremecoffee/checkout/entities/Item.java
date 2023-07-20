1f47fa3a5d269265002be4548b4a51b14991dff0
Vincent Dlugosch <dlugoschvincent@gmail.com>
2023-07-20 14:55:42 +0200

    fix: move equals method to itemid

--- src/main/java/de/extremecoffee/checkout/entities/Item.java
+++ src/main/java/de/extremecoffee/checkout/entities/Item.java
@@ -10,15 +10,10 @@ import java.util.Objects;
 @Entity
 @IdClass(ItemId.class)
 public class Item extends PanacheEntityBase {
-  @Id
-  public Long bagSizeId;
+	@Id
+	public Long bagSizeId;
 
-  @Id
-  public Long productId;
+	@Id
+	public Long productId;
 
-  @Override
-  public boolean equals(Object o) {
-    Item item = (Item) o;
-    return Objects.equals(item.bagSizeId, this.bagSizeId) && Objects.equals(item.productId, this.productId);
-  }
 }
--- src/main/java/de/extremecoffee/checkout/entities/ItemId.java
+++ src/main/java/de/extremecoffee/checkout/entities/ItemId.java
@@ -3,7 +3,13 @@ package de.extremecoffee.checkout.entities;
 import java.io.Serializable;
 
 public class ItemId implements Serializable {
-  public Long bagSizeId;
+	public Long bagSizeId;
 
-  public Long productId;
+	public Long productId;
+
+	@Override
+	public boolean equals(Object o) {
+		ItemId itemId = (ItemId) o;
+		return itemId.bagSizeId == this.bagSizeId && itemId.productId == this.productId;
+	}
 }
